441b4145a7fef9c9d4ce021bad01ab62fc495a2b
Alexander Bruy <alexander.bruy@gmail.com>
2010-12-13 19:09:12 +0000

    implement plugin manager icon support

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-mVersion = "0.1.0"
+mVersion = "0.1.1"
 def name():
   return "Quickly save default qml"
 def description():
@@ -10,6 +10,8 @@ def version():
   return mVersion
 def authorName():
   return "Maxim Dubinin (sim@gis-lab.info)"
+def icon():
+  return "icon.png"
 def classFactory(iface):
   from quicksaveqml import QuickSaveQml
   return QuickSaveQml(iface)
