a4f702a86eafe1874b520c543e00b051cf21ea77
Valeriy Remeshevskiy <remeshevskiy@gmail.com>
2024-10-19 13:47:25 +0300

    Replace exec_ with exec and pre-commit formatting

--- src/quicksaveqml/quicksaveqml.py
+++ src/quicksaveqml/quicksaveqml.py
@@ -11,43 +11,57 @@ import os
 from . import quicksaveqmldialog
 from .compat import map_layers
 
+
 class QuickSaveQml:
+    def __init__(self, iface):
+        """Initialize the class"""
+        self.iface = iface
 
-  def __init__( self, iface ):
-    """Initialize the class"""
-    self.iface = iface
+    def initGui(self):
+        self.action = QAction(
+            QIcon(":/plugins/quicksaveqml/icons/icon.png"),
+            "Save default QML",
+            self.iface.mainWindow(),
+        )
+        self.action.setStatusTip(
+            "Click to save qml file as default (with the same as a layer)"
+        )
+        self.action.setWhatsThis("Save QML files as default")
 
-  def initGui( self ):
-    self.action = QAction( QIcon( ":/plugins/quicksaveqml/icons/icon.png"), "Save default QML", self.iface.mainWindow() )
-    self.action.setStatusTip( "Click to save qml file as default (with the same as a layer)" )
-    self.action.setWhatsThis( "Save QML files as default" )
+        self.actionBatch = QAction(
+            QIcon(":/plugins/quicksaveqml/batch_save.png"),
+            "Save default QML (batch)",
+            self.iface.mainWindow(),
+        )
+        self.actionBatch.setStatusTip(
+            "Click to save qml files as default (with the same as a layer)"
+        )
+        self.actionBatch.setWhatsThis("Save QML files as default (batch)")
 
-    self.actionBatch = QAction(QIcon( ":/plugins/quicksaveqml/batch_save.png" ), "Save default QML (batch)", self.iface.mainWindow() )
-    self.actionBatch.setStatusTip( "Click to save qml files as default (with the same as a layer)" )
-    self.actionBatch.setWhatsThis( "Save QML files as default (batch)" )
+        self.action.triggered.connect(self.run)
+        self.actionBatch.triggered.connect(self.runBatch)
 
-    self.action.triggered.connect(self.run)
-    self.actionBatch.triggered.connect(self.runBatch)
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addToolBarIcon(self.actionBatch)
 
-    self.iface.addToolBarIcon( self.action )
-    self.iface.addToolBarIcon( self.actionBatch )
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        self.iface.removeToolBarIcon(self.actionBatch)
 
-  def unload( self ):
-    self.iface.removeToolBarIcon( self.action )
-    self.iface.removeToolBarIcon( self.actionBatch )
+    def run(self):
+        layersmap = map_layers()
+        layerslist = []
+        curLayer = self.iface.mapCanvas().currentLayer()
+        if curLayer == None:
+            QMessageBox.information(
+                self.iface.mainWindow(), "Warning", "No layers selected"
+            )
+            return
+        curLayerName = curLayer.source()
+        basename = os.path.splitext(str(curLayerName))[0]
+        curLayerQMLName = basename + ".qml"
+        curLayer.saveNamedStyle(curLayerQMLName)
 
-  def run( self ):
-    layersmap = map_layers()
-    layerslist = []
-    curLayer = self.iface.mapCanvas().currentLayer()
-    if curLayer == None:
-      QMessageBox.information( self.iface.mainWindow(), "Warning", "No layers selected" )
-      return
-    curLayerName = curLayer.source()
-    basename = os.path.splitext( str( curLayerName ) )[ 0 ]
-    curLayerQMLName = basename + ".qml"
-    curLayer.saveNamedStyle( curLayerQMLName )
-
-  def runBatch( self ):
-    dlg = quicksaveqmldialog.QuickSaveQmlDialog( self.iface )
-    dlg.exec_()
+    def runBatch(self):
+        dlg = quicksaveqmldialog.QuickSaveQmlDialog(self.iface)
+        dlg.exec()
